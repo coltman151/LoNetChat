11683bba1e4023ae3d70a7775e7323fcf457a273
Colton <charrill@pm.me>
2020-12-19 20:43:50 -0500

    add gitignore

--- Server.py
+++ Server.py
@@ -1,12 +1,13 @@
 import socket
 
-s = socket.socket()
-port = 12345
-s.bind(('', port))
+def server():
+    s = socket.socket()
+    port = 12345
+    s.bind(('', port))
 
-s.listen(5)
-while True:
-    c, addr = s.accept()
-    print('Got connection from', addr)
-    c.send(b'thank you for connectiong')
-    c.close()
+    s.listen(5)
+    while True:
+        c, addr = s.accept()
+        print('Got connection from', addr)
+        c.send(b'thank you for connectiong')
+        c.close()
--- client.py
+++ client.py
@@ -1,14 +1,15 @@
 import socket
 
-s = socket.socket()
-host = input("Enter an IP Address:\n>>>")
-print(host)
-port = 12345
+def client():
+    s = socket.socket()
+    host = input("Enter an IP Address:\n>>>")
+    print(host)
+    port = 12345
 
-try:
-    s.settimeout(5)
-    s.connect((host, port))
-    print(s.recv(1024))
-    s.close()
-except socket.timeout:
-    print("Connection Timed Out")
+    try:
+        s.settimeout(5)
+        s.connect((host, port))
+        print(s.recv(1024))
+        s.close()
+    except socket.timeout:
+        print("Connection Timed Out")
